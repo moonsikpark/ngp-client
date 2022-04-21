19acf6ce543b15feded22cb43b5d1ca16cfa023d
Moonsik Park <mooonsik.park@gmail.com>
2022-04-21 23:05:50 +0900

    Add protobuf library.

--- routes/vendors.js
+++ routes/vendors.js
@@ -4,5 +4,6 @@ var router = express.Router();
 
 router.use('/bootstrap', express.static(path.join(__dirname, "../node_modules/bootstrap/dist")));
 router.use('/jquery', express.static(path.join(__dirname, "../node_modules/jquery/dist")));
+router.use('/protobufjs', express.static(path.join(__dirname, "../node_modules/protobufjs/dist")));
 
 module.exports = router;
