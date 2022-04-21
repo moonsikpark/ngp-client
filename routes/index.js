da6d43fd8254217e50b8399ba765ab5a64edc929
Moonsik Park <mooonsik.park@gmail.com>
2022-04-21 16:44:57 +0900

    Add /povcontrol.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/povcontrol', function (req, res, next) {
+  res.render('povcontrol', { title: 'POV Control' });
 });
 
 module.exports = router;
