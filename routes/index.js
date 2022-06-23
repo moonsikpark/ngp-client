8f2f1d7a4bda75c9e42456cacdfbe1cbdd0fed7f
Moonsik Park <mooonsik.park@gmail.com>
2022-06-23 13:57:22 +0900

    Edit three & bump others.

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,30 @@ router.get('/povcontrol', function (req, res, next) {
   res.render('povcontrol', { title: 'POV Control' });
 });
 
+router.get('/webcodec', function (req, res, next) {
+  res.render('webcodec', { title: 'WebCodec test' });
+});
+
+router.get('/webxr', function (req, res, next) {
+  res.render('webxr', { title: 'WebXR test' });
+});
+
+router.get('/resolution', function (req, res, next) {
+  res.render('resolution', { title: 'resolution test' });
+});
+
+router.get('/resolution2', function (req, res, next) {
+  res.render('resolution2', { title: 'resolution test' });
+});
+
+
+router.get('/shader', function (req, res, next) {
+  res.render('shader', { title: 'resolution test' });
+});
+
+router.get('/mediastream', function (req, res, next) {
+  res.render('mediastream', { title: 'resolution test' });
+});
+
+
 module.exports = router;
